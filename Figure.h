8f9accead8e6555f684d5e8644b90f95579e8fe0
521370910130 <yjl2030906598@sjtu.edu.cn>
2021-12-03 18:16:38 +0800

    Figure-ColoredFig

---
+++ Figure.h
@@ -0,0 +1,76 @@
+#ifndef FIGURE_H
+#define FIGURE_H
+#include "Vector.h"
+#include <GL/glut.h>
+class Figure
+{
+public:
+	Figure() : anchor(0, 0) {}
+	Vec getAnchor() { return anchor; }
+	void setAnchor(Vec a) { anchor = a; }
+	virtual void draw() = 0;
+	virtual void move(Vec dir) = 0;
+	virtual void rotate(float angle) = 0;
+	virtual void zoom(float k) = 0;
+	virtual ~Figure() {}
+
+protected:
+	Vec anchor;
+};
+class ColoredFig : public Figure
+{
+public:
+	virtual void draw() = 0;
+
+protected:
+	double r, g, b;
+};
+class Quodrilateral : public ColoredFig
+{
+public:
+	virtual void draw() = 0;
+
+protected:
+};
+class Rectangle : public Quodrilateral
+{
+public:
+	Rectangle(Vec *v1, Vec *v2, double r = 0, double g = 0, double b = 0);
+	void draw();
+	void zoom(double k);
+
+protected:
+	Vec *v1, *v2;
+};
+class Trapezium : public Quodrilateral
+{
+public:
+	Trapezium(Vec *v1, Vec *v2, Vec *v3, Vec *v4, double r = 0, double g = 0, double b = 0);
+	void draw();
+	void zoom(double k);
+
+protected:
+	Vec *v1, *v2, *v3, *v4;
+};
+class Triangle : public ColoredFig
+{
+public:
+	Triangle(Vec *v1, Vec *v2, Vec *v3, double r = 0, double g = 0, double b = 0);
+	void draw();
+	void zoom(double k);
+
+protected:
+	Vec *v1, *v2, *v3;
+};
+class Polygon : public ColoredFig
+{
+public:
+	Polygon(int n = 4, double R = 1, double r = 0, double g = 0, double b = 0);
+	void draw();
+	void zoom(double k);
+
+protected:
+	int n;
+	double R;
+};
+#endif
