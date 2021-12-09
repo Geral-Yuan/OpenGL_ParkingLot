2a891e4fea95ea4a1718f70bff4ca81271d6c92f
liujiaren <ljr030815@sjtu.edu.cn>
2021-12-09 18:05:49 +0800

    p1

---
+++ p3p1.h
@@ -0,0 +1,51 @@
+#include<ctime>
+#include<iostream>
+#include <cstdlib>
+
+#ifndef P3P1_H
+#define P3P1_H
+class Vehicle {
+    public:
+        void generate();
+        void remove();
+        virtual void printenterticket();
+        virtual void printexitticket(int exittime);
+        virtual int countprice(int exittime);
+    
+    protected:
+        int entertime;
+        int slotnum;
+};
+
+class Car : public Vehicle {
+    public:
+        int countprice(int exittime);
+        void printenterticket();
+        void printexitticket(int exittime);
+};
+
+class Van : public Vehicle {
+    public:
+        int countprice(int exittime);
+        void printenterticket();
+        void printexitticket();
+};
+
+
+class Motor : public Vehicle {
+    public:
+        int countprice(int exittime);
+        void printenterticket();
+        void printexitticket();
+};
+
+
+class Bike : public Vehicle {
+    public:
+        int countprice(int exittime);
+        void printenterticket();
+        void printexitticket();
+};
+
+
+#endif
