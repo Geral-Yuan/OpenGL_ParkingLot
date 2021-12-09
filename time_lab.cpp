ea52437b400eed564fb834d633c5d3d634a6e4b7
521370910089 <ganhanyu@sjtu.edu.cn>
2021-12-09 17:41:58 +0800

    a solution to time-related problems

--- time_lab.cpp
+++ time_lab.cpp
@@ -3,12 +3,42 @@
 //
 #include <iostream>
 #include <ctime>
-#include <cstdint>
-using namespace std;
+#include <chrono>
 
+using namespace std;
+void printLocalTimeDetail();
+void printLocalTimePithy();
+int delaying(int timed);
+void threeWayPrintTime();
+void testAddTime(size_t num);
+void chronoDelay(double second);
 int main()
 {
+    printLocalTimePithy();
+    //threeWayPrintTime();
+    //testAddTime(10000000);
+    cout << "---------------------------------" << endl;
+    delaying(5);
+    printLocalTimePithy();
+    cout << "---------------------------------" << endl;
+    chronoDelay(5);
+    printLocalTimePithy();
 
+    return 0;
+}
+
+int delaying(int timed)
+{
+    time_t timedd = (time_t) timed;
+    const time_t ts = time(0);
+    time_t te = 0;
+    while(te==0 || te - ts <= timedd)
+    {
+        te = time(0);
+    }
+    return 1;
+}
+void printLocalTimeDetail(){
     time_t t = time(0);
     /*get the present time in second since the epoch
     time type is int type of the second since 1970/1/1
@@ -19,24 +49,25 @@ int main()
     //give a pointer to the second since the epoch, and computer will change it into our local time
     cout << endl <<" Display the Current Date and Time :" << endl;
     {
-    cout << " seconds = " << localtimepointer->tm_sec << endl;
-    cout << " minutes = " << localtimepointer->tm_min << endl;
-    cout << " hours = " << localtimepointer->tm_hour << endl;
-    cout << " day of month = " << localtimepointer->tm_mday << endl;
-    cout << " month of year = " << localtimepointer->tm_mon + 1 << endl;
-    cout << " year = " << localtimepointer->tm_year + 1900 << endl;
-    /*start from 1900*/
-    cout << " weekday = " << localtimepointer->tm_wday << endl;
-    cout << " day of year = " << localtimepointer->tm_yday << endl;
-    //cout << " daylight savings = " <<(localtimepointer->tm_isdst )<< endl;
-    cout << endl;
+        cout << " seconds = " << localtimepointer->tm_sec << endl;
+        cout << " minutes = " << localtimepointer->tm_min << endl;
+        cout << " hours = " << localtimepointer->tm_hour << endl;
+        cout << " day of month = " << localtimepointer->tm_mday << endl;
+        cout << " month of year = " << localtimepointer->tm_mon + 1 << endl;
+        cout << " year = " << localtimepointer->tm_year + 1900 << endl;
+        /*start from 1900*/
+        cout << " weekday = " << localtimepointer->tm_wday << endl;
+        cout << " day of year = " << localtimepointer->tm_yday << endl;
+        //cout << " daylight savings = " <<(localtimepointer->tm_isdst )<< endl;
+        cout << endl;
     }
 
     cout << " Current Date: " << (localtimepointer->tm_mday) << "/" << (localtimepointer->tm_mon) + 1 << "/" << (localtimepointer->tm_year) + 1900 << endl;
     cout << " Current Time: " << (localtimepointer->tm_hour) << ":" << (localtimepointer->tm_min) << ":" << (localtimepointer->tm_sec) << endl;
     cout << endl;
-
-
+}
+void threeWayPrintTime() {
+    time_t t = time(0);
     if (t != (time_t) (-1))
     {
         cout << "The current time is " << asctime(localtime(&t)) << (intmax_t) t << " seconds since the Epoch" << endl;
@@ -44,11 +75,44 @@ int main()
         //asctime: ASCII time
         cout << "The current time is " << ctime(&t) << (intmax_t) t << " seconds since the Epoch" << endl;
     }
-    return 0;
+}
+void testAddTime(size_t num)
+{
+    time_t t = time(0);
+    size_t sum = 0;
+    for (size_t i = 0; i <= num; i++)
+    {
+        sum+=i;
+    }
+    cout << sum << endl;
+    time_t t2 = time(0);
+    cout << (intmax_t)t2-t << endl;
+}
+void chronoDelay(double second)
+{
+    const chrono::time_point start = chrono::system_clock::now();
+    chrono::time_point end = chrono::system_clock::now();
+    chrono::duration<double> diff = end - start;
+    while( (double)diff.count() <= second)
+    {
+        end = chrono::system_clock::now();
+        diff = end - start;
+    }
+}
+void printLocalTimePithy(){
+    time_t t = time(0);
+    tm* localtimepointer = localtime(&t);
+    cout << " Current Date: " << (localtimepointer->tm_mday) << "/" << (localtimepointer->tm_mon) + 1 << "/" << (localtimepointer->tm_year) + 1900 << endl;
+    cout << " Current Time: " << (localtimepointer->tm_hour) << ":" << (localtimepointer->tm_min) << ":" << (localtimepointer->tm_sec) << endl;
+    cout << endl;
 }
 //reference
 /*
 https://en.cppreference.com/w/c/chrono/tm
 https://en.cppreference.com/w/c/chrono/time
 https://www.w3resource.com/cpp-exercises/basic/cpp-basic-exercise-34.php
+https://en.cppreference.com/w/cpp/chrono/system_clock/now
  */
+
+
+
