8788577611e502a0f60fe7c9f7c8c19b52f356ad
liujiaren <ljr030815@sjtu.edu.cn>
2021-12-10 21:17:41 +0800

    11

--- p3p1.h
+++ p3p1.h
@@ -1,52 +1,76 @@
+//
+// Created by ljr and ghy on 2021/12/9.
+// Updated by ghy on 2021/12/10 12:06
+//
 #ifndef P3P1_H
 #define P3P1_H
-
-#include<ctime>
-#include<iostream>
-#include <cstdlib>
+#include <ctime>//time related functions and data types (might not use)
+#include <iostream>//i/o
+#include <cstdlib>//standard library, many useful tools
+#include <chrono>//time related functions and data types
+#include <vector>//It is highly recommended to use vector to store pointers to the vehicles
+using namespace std;
 
 class Vehicle {
-    public:
-        void generate();
-        void remove();
-        virtual void PrintEnterTicket();
-        virtual void PrintExitTicket(int exittime);
-        virtual int CountPrice(int exittime);
-    
-    protected:
-        int entertime;
-        int slotnum;
-};
+public:
+    virtual ~Vehicle();
+    virtual void PrintEnterTicket() = 0;
+    virtual void PrintExitTicket() = 0;
+protected:
+    //chrono::time_point<chrono::system_clock> enterTime;
+    time_t enterTimeSec;
+    //int slotNum;
+};//abstract class
 
 class Car : public Vehicle {
-    public:
-        int CountPrice(int exittime);
-        void PrintEnterTicket();
-        void PrintExitTicket(int exittime);
+public:
+    Car();
+    ~Car();
+    void PrintEnterTicket() override;
+    void PrintExitTicket() override;
 };
 
 class Van : public Vehicle {
-    public:
-        int CountPrice(int exittime);
-        void PrintEnterTicket();
-        void PrintExitTicket(int exittime);
+public:
+    Van();
+    ~Van();
+    void PrintEnterTicket() override;
+    void PrintExitTicket() override;
 };
-
 
 class Motor : public Vehicle {
-    public:
-        int CountPrice(int exittime);
-        void PrintEnterTicket();
-        void PrintExitTicket(int exittime);
+public:
+    Motor();
+    ~Motor();
+    void PrintEnterTicket() override;
+    void PrintExitTicket() override;
 };
-
 
 class Bike : public Vehicle {
-    public:
-        int CountPrice(int exittime);
-        void PrintEnterTicket();
-        void PrintExitTicket(int exittime);
+public:
+    Bike();
+    ~Bike();
+    void PrintEnterTicket() override;
+    void PrintExitTicket() override;
+};
+class parkingLot{
+public:
+    parkingLot();
+    ~parkingLot();
+    int getSize();
+    Vehicle *GetVehicle(int vehicleIndex);
+    void removeVehicle(int vehicleIndex);
+    void vehicleTypeDice();
+    void vehicleIndexDice();
+private:
+    vector<Vehicle*> vehicleVector;
+    //vehicleVector parking lot has vehicleVector container of vehicles' information (implemented as vehicles' pointers)
 };
 
+void chronoDelay(double second);//delay vehicleVector certain time, using second(can be double type)
+void printLocalTimePithy();//print local time to debug;
+
+int actionDice(double a_LARGER_0_0001,double b_LARGER_0_0001);
+//a dice to decide action (input e.g. chanceA = 0.001(return 1), chanceB = 0.01(return 2), else(return 0))
 
 #endif
