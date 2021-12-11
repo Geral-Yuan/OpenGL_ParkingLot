d52c3036ab4e843a6125f8208098c76e802ab400
521370910130 <yjl2030906598@sjtu.edu.cn>
2021-12-11 21:03:47 +0800

    Makefile

---
+++ src/ipa.cpp
@@ -0,0 +1,60 @@
+#include "parking_lot.h"
+
+void TimeStep(int n)
+{
+	glutTimerFunc(25, TimeStep, n);
+	glutPostRedisplay();
+}
+void glDraw()
+{
+	single_park *carpark_map = single_park::get_instance();
+	static int t = 0;
+	//drawing part
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	//draw the parking lot
+	carpark_map->draw();
+	//generate a new vehicle
+	if (t % 300 == 0)
+		carpark_map->generate_vehicle();
+	carpark_map->move_barrier();
+	//move and draw the vehicle
+	carpark_map->move_vehicle();
+	glutSwapBuffers();
+	glFlush();
+	t++;
+}
+
+int main(int argc, char *argv[])
+{
+	//use commandline argument to specify the number of slots per row
+	int slot_num_per_row = 6;
+	char c = 0;
+	while ((c = getopt(argc, argv, "n:")) != -1)
+		switch (c)
+		{
+		case 'n':
+			slot_num_per_row = atoi(optarg);
+			break;
+		}
+	//initialize a singleton parking lot with its slot number per row
+	single_park *carpark_map = single_park::get_instance();
+	carpark_map->set_slot(slot_num_per_row);
+	glutInit(&argc, argv);
+	//set the window size accoring to user input
+	glutInitWindowSize((20 * slot_num_per_row + 80) * 3, 510);
+	glutInitWindowPosition(0, 0);
+	glutInitDisplayMode(GLUT_RGB);
+	//create the window and clear the background
+	glutCreateWindow("Interstellar parking animation");
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	//set the range of the x&y axis
+	gluOrtho2D(-40, 20 * slot_num_per_row + 40, -50, 120);
+	srand((unsigned int)time(NULL));
+	glutDisplayFunc(glDraw);
+	glutTimerFunc(25, TimeStep, 25);
+	glutMainLoop();
+	carpark_map->delete_parking_lot();
+	carpark_map->delete_instance();
+	return 0;
+}
---
+++ src/parking_lot.h
@@ -0,0 +1,107 @@
+//
+//  parking_lot.h
+//
+#ifndef PARKING_LOT_H
+#define PARKING_LOT_H
+//system judge
+#ifdef __APPLE__
+#define GL_SILENCE_DEPRECATION
+#include <GLUT/glut.h>
+#elif _WIN32
+#include <GL/freeglut.h>
+#include <windows.h>
+#elif _WIN64
+#include <GL/freeglut.h>
+#include <windows.h>
+#elif __linux__
+#include <GL/freeglut.h>
+#endif
+
+//C++ libraries
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <getopt.h>
+using namespace std;
+
+//define vector class for anchors
+#include "vector.h"
+
+//define all figures
+#include "figure.h"
+
+//draw the parking area
+void parking_area_draw();
+
+//singleton trial
+class single_park{
+private:
+    int slot_num;
+	int empty_slot_num;
+    vector<Vehicle*> all_vehicles;
+    int* slots_order;
+	int bar_open_time=0;
+    single_park(){}
+    ~single_park(){}
+    //delete vehicle
+    void delete_vehicle();
+    //free the memory allocated
+    void delete_slot(){
+        delete[] slots_order;
+    }
+public:
+    static single_park* get_instance(){
+        static single_park *parking_lot = NULL;
+        if (parking_lot == NULL)
+            parking_lot = new single_park();
+        return parking_lot;
+    }
+    static void delete_instance(){
+        single_park* parking_lot = single_park::get_instance();
+        if(parking_lot != NULL){
+            delete parking_lot;
+            parking_lot = NULL;
+        }
+    }
+    //set the slot per row
+    void set_slot(int _slot){
+        slot_num = 2 * _slot;
+		empty_slot_num = slot_num;
+        slots_order = new int[slot_num]();
+		for (int i = 0; i < slot_num; i++)
+			slots_order[i] = i;
+    }
+    //draw the car park
+    void draw();
+    //generate new vehicle
+    void generate_vehicle();
+    //move vehicle
+	void move_vehicle();
+    //delete the parking lot
+	void move_barrier(){
+		glColor3d(1, 0, 0);
+		glBegin(GL_LINES);
+		glVertex2d(0, 0);
+		if (bar_open_time > 0)
+		{
+			if (bar_open_time > 70)
+				glVertex2d(-25 * cos(PI / 40 * (90 - bar_open_time)), 25 * sin(PI / 40 * (90 - bar_open_time)));
+			else if (bar_open_time <= 20)
+				glVertex2d(-25 * cos(PI / 40 * bar_open_time), 25 * sin(PI / 40 * bar_open_time));
+			else
+				glVertex2d(0, 25);
+			bar_open_time -= 1;
+		}
+		else
+			glVertex2d(-25, 0);
+		glEnd();
+	}
+    void delete_parking_lot(){
+        delete_vehicle();
+        delete_slot();
+    }
+};
+
+#endif /* PARKING_LOT_H */
